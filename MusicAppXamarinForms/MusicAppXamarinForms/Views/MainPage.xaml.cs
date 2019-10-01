cd5d2e70b2958f5dbf3e0dc321e9dd73d2647f65
Adam Anderson <andersad@gmail.com>
2019-10-01 13:38:15 -0400

    Add a parallax scroll effect to now playing image

--- MusicAppXamarinForms/MusicAppXamarinForms/Views/MainPage.xaml.cs
+++ MusicAppXamarinForms/MusicAppXamarinForms/Views/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,18 @@ namespace MusicAppXamarinForms.Views
     public partial class MainPage : ContentPage
     {
         private bool isPaused;
+        private double bandImageStartPosition;
 
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            this.bandImageStartPosition = this.BandImageView.TranslationY;
         }
 
         private void AnimationView_OnOnClick(object sender, EventArgs e)
@@ -22,6 +31,11 @@ namespace MusicAppXamarinForms.Views
                 this.isPaused ? 0.5f : 0.0f,
                 this.isPaused ? 1.0f : 0.5f);
             this.isPaused = !this.isPaused;
+        }
+
+        private void ScrollView_OnScrolled(object sender, ScrolledEventArgs e)
+        {
+            this.BandImageView.TranslationY = bandImageStartPosition + e.ScrollY * 0.2;
         }
     }
 }
